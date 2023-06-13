45d7b8d15cbdc3c3ae48930ed6e8347881f0849b
nabtc1997 <98686587+nabtc1997@users.noreply.github.com>
2023-06-13 19:02:57 +0200

    Create wallet.py
    Wallet check

---
+++ wallet.py
@@ -0,0 +1,8 @@
+def evm_wallet(key):
+
+    try:
+        web3 = Web3(Web3.HTTPProvider(DATA['ethereum']['rpc']))
+        wallet = web3.eth.account.from_key(key).address
+        return wallet
+    except:
+        return key
